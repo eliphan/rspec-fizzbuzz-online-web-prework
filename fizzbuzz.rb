1207b3d4750cace77ec894830ae10109abf3fa7c
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-29 00:55:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 15 == 0 
+      "FizzBuzz"  
   elsif int % 5 == 0  
      "Buzz"
-  else 
+  else int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
   end
 end
