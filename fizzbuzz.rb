9541bdf59b74538c0a5f4f3b4a741e67dafcde5f
generic-thread-7687 <aliceconnic@gmail.com>
2018-10-29 01:00:35 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@
 def fizzbuzz(int)
   if int % 15 == 0 
       "FizzBuzz"
-  elsif int % 5 != 0 || int % 3 != 0
+  elsif int % 5 != 0 && int % 3 != 0
       "nil"
   elsif int % 5 == 0  
      "Buzz"
